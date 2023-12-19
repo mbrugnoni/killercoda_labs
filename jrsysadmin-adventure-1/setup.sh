5fa00e5fa00510be7ee60cb9cbf0c5ae37a4f1ab
mbrugnoni <michael.brugnoni@gmail.com>
2023-12-18 19:56:51 -0500

    update

--- jrsysadmin-adventure-1/setup.sh
+++ jrsysadmin-adventure-1/setup.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-printf 'This is a bash shell script.\n\n'
-printf 'Any commands you put in here will be executed at runtime.\n\n'
-printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
-printf 'The file is named foreground.sh because when it executes, the commands are echoed to the terminal.\n\n'
-printf 'To run a script in the background, so learners will not see it execute, specify it in the index.json as "background": "background.sh"\n\n'
+useradd -m bblursky -p forgotten
+mkdir /home/bblursky/'Family Pictures and Videos'
+mkdir /home/bblursky/'work'
+(echo "st@rting252"; echo "summer2023"; echo "winter2022"; echo "Sally04122015"; echo 'h@t3myB0$$') > /home/bblursky/passwords.txt
+

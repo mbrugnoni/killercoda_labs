33dadc656766d1620cb5c6c6f2f85a84c8812133
mbrugnoni <michael.brugnoni@gmail.com>
2024-06-16 19:34:10 -0400

    update

--- jrsysadmin-adventure-3/step3/verify.sh
+++ jrsysadmin-adventure-3/step3/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if grep -P "appadmin\s+ALL=ALL\s+NOPASSWD:\s+/apps/install\.sh" /etc/sudoers; then
+if grep -P "appadmin\s+ALL=\(ALL\)\s+NOPASSWD:\s+/apps/install\.sh" /etc/sudoers || grep -P "appadmin\s+ALL=\(ALL:ALL\)\s+NOPASSWD:\s+/apps/install\.sh" /etc/sudoers ; then
     /bin/true
 else
     /bin/false

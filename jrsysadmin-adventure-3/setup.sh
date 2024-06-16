d017edd37c47f25da3403a2a1775f9c09398542f
mbrugnoni <michael.brugnoni@gmail.com>
2024-06-16 19:10:57 -0400

    update

--- jrsysadmin-adventure-3/setup.sh
+++ jrsysadmin-adventure-3/setup.sh
@@ -9,7 +9,7 @@ chown root:finance-devs /apps/finance
 mkdir /home/bblursky/'Family Pictures and Videos'
 mkdir /home/bblursky/work
 echo "# Add my customizations - remember to finish later when I get back online" >> /home/bblursky/.bashrc
-echo 'PATH=""' >> /home/bblursky/.bashrc
+echo 'export PATH=""' >> /home/bblursky/.bashrc
 (echo "st@rting252"; echo "summer2023"; echo "winter2022"; echo "Sally04122015"; echo 'h@t3myB0$$') > /home/bblursky/passwords.txt
 mkdir -p /tmp/6487
 echo "bblursky important work" > /tmp/6487/important.doc
